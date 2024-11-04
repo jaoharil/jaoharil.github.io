3f2ad6ac3e0d9291b80936499d681df99dc3b829
jaoharil <jaoharilmaknun2904@gmail.com>
2024-11-04 15:01:15 +0530

    add to content projeck

--- js/projeck.js
+++ js/projeck.js
@@ -1,13 +1,48 @@
-function showLoading(event, url) {
-  event.preventDefault();
+const slider = document.querySelector('.slider');
+const list = document.querySelector('.list');
+const thumbnail = document.querySelector('.thumbnail');
+const next = document.querySelector('#next');
+const prev = document.querySelector('#prev');
 
-  const loadingOverlay = document.querySelector('.fullscreen-loading-overlay');
+// autoplay slider
+let runAutoPlay = setTimeout(() => {
+  next.clik();
+}, 7000);
 
-  // Tampilkan overlay loading fullscreen
-  loadingOverlay.classList.add('show');
+next.addEventListener('click', () => {
+  initSlider('next');
+});
+prev.addEventListener('click', () => {
+  initSlider('prev');
+});
 
-  // Tunggu beberapa detik, lalu arahkan ke link
+const initSlider = (type) => {
+  const sliderItems = list.querySelectorAll('.item');
+  const thumbnailItems = thumbnail.querySelectorAll('.item');
+
+  if (type === 'next') {
+    // membuat list item nya ke belakang
+    list.appendChild(sliderItems[0]);
+    // membuat thumbnail nya ke belakang
+    thumbnail.appendChild(thumbnailItems[0]);
+    // mengasih slider nya class baru yaitu next
+    slider.classList.add('next');
+  } else {
+    // nyari posisi terkahir list itemnya
+    const lastItemsPosition = sliderItems.length - 1;
+    //
+    list.prepend(sliderItems[lastItemsPosition]);
+    thumbnail.prepend(thumbnailItems[lastItemsPosition]);
+    slider.classList.add('prev');
+  }
+
   setTimeout(() => {
-    window.location.href = url;
-  }, 1000); // Waktu loading 1 detik
-}
+    slider.classList.remove('next');
+    slider.classList.remove('prev');
+  }, 2000);
+
+  clearTimeout(runAutoPlay);
+  runAutoPlay = setTimeout(() => {
+    next.clik();
+  }, 7000);
+};
